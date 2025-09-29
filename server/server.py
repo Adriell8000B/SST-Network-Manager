91db8c0ddb6e6feb668b102546027f6168924004
Adriell8000B <adrielluiz73@gmail.com>
2025-09-29 11:47:16 -0300

    Created server structure

---
+++ server/server.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from abstracts.abstracts import IRouter
+
+class Server:
+	def __init__(self, flask: Flask, router: IRouter, PORT: int) -> None:
+		self._flask = flask
+		self._router = router
+		self._PORT = PORT
+	
+	def _setup(self) -> None:
+		self._router.setup_routes()
+	
+	def _listen(self):
+		try:
+			self._flask.run(None, self._PORT)
+		except Exception as error:
+			print(f"Coudln't start the server due to an error: {error}")
+
+	def init(self):
+		self._setup()
+		self._listen()
