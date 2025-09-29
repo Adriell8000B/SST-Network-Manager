44cbee7d9ab00e3c3218495d0b2ec1135386ab19
Adriell8000B <adrielluiz73@gmail.com>
2025-09-29 16:49:26 -0300

    Add uri setup

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from database.database import Database
 from router.router import Router
 from router.routes import blueprint
 from server.server import Server
 
 flask = Flask(__name__)
 router = Router(flask, blueprint)
-server = Server(flask, router, 3333)
+database = Database("")
+server = Server(flask, router, database,3333)
 
 server.init()
