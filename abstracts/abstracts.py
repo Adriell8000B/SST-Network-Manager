8337d211f63d8350a6675021d81dda01d411b251
Adriell8000B <adrielluiz73@gmail.com>
2025-09-29 15:48:00 -0300

    Added mongo_factory

--- abstracts/abstracts.py
+++ abstracts/abstracts.py
@@ -1,6 +1,12 @@
+from pymongo import AsyncMongoClient
 from abc import ABC, abstractmethod
 
 class IRouter(ABC):
 	@abstractmethod
 	def setup_routes(self):
+		pass
+
+class IMongoFactory(ABC):
+	@abstractmethod
+	def create_async_client(self) -> AsyncMongoClient:
 		pass
