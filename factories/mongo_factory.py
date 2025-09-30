1d8364d56c7608535b99dd697a85cda4b4b78236
Adriell8000B <adrielluiz73@gmail.com>
2025-09-30 11:32:27 -0300

    Remove mongo_factory

--- factories/mongo_factory.py
+++
@@ -1,7 +0,0 @@
-from pymongo import AsyncMongoClient
-from abstracts.abstracts import IMongoFactory
-
-class MongoFactory(IMongoFactory):
-	@staticmethod
-	def create_async_client(uri: str) -> AsyncMongoClient:
-		return AsyncMongoClient(uri)
