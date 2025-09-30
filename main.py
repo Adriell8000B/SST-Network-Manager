553278aa29e90c5cbe85c26196f046f82a997f49
Adriell8000B <adrielluiz73@gmail.com>
2025-09-30 10:30:16 -0300

    Remove hardcoded attributes

--- main.py
+++ main.py
@@ -3,10 +3,18 @@ from database.database import Database
 from router.router import Router
 from router.routes import blueprint
 from server.server import Server
+from utils.utils import get_env_string, setup_environment
+
+setup_environment()
 
 flask = Flask(__name__)
 router = Router(flask, blueprint)
-database = Database("")
-server = Server(flask, router, database,3333)
+database = Database(get_env_string("MONGODB_URI"))
+server = Server(
+	flask,
+	router,
+	database,
+	int(get_env_string("PORT"))
+)
 
 server.init()
