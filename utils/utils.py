0ebd923bcedf910ebaf780c07307fe3b063cc42f
Adriell8000B <adrielluiz73@gmail.com>
2025-09-30 10:29:56 -0300

    Added utils.py

---
+++ utils/utils.py
@@ -0,0 +1,17 @@
+from os import getenv
+from dotenv import load_dotenv
+
+def setup_environment():
+	env: str | None = getenv("ENV")
+
+	if env == "DEV":
+		print("Development environment detected!\nLoading dotenv...")
+		load_dotenv()
+
+def get_env_string(env: str) -> str:
+	temp: str | None = getenv(env)
+
+	if temp == None:
+		return ""
+	
+	return temp
