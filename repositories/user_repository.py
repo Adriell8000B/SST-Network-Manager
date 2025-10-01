b22119e207792e297fe9c5656493b960f3bd67a2
Adriell8000B <adrielluiz73@gmail.com>
2025-10-01 10:49:24 -0300

    Finish repository implementation

--- repositories/user_repository.py
+++ repositories/user_repository.py
@@ -1,16 +1,18 @@
-from typing import List
-from prisma.models import users
-from abstracts.abstracts import IUserRepository
-from prisma import Prisma
+from typing import Any, List
 
+from prisma import Prisma
+from prisma.models import users
+
+from abstracts.abstracts import IUserRepository
+	
 class UserRepository(IUserRepository):
 	def __init__(self, prisma: Prisma) -> None:
 		self._prisma = prisma
 
-	async def retrieve_users(self) -> List[users]:
+	async def retrieve_users(self) -> list[dict[str, Any]]:
 		response = await self._prisma.users.find_many()
 
 		if response != List[users]:
-			print(f"Couldn't get users")
+			print(f"Couldn't retrieve users")
 
-		return response
+		return [user.model_dump() for user in response]
