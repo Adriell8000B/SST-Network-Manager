55bf2eb3051cd84da7eab4f20e7ba5f40060f0ed
Adriell8000B <adrielluiz73@gmail.com>
2025-10-01 10:49:52 -0300

    Update user_controller core method

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -1,10 +1,18 @@
-from typing import Any, Coroutine, List
-from prisma.models import users
+from typing import Any
+from flask import Response, jsonify
+
 from abstracts.abstracts import IUserController, IUserRepository
 
 class UserController(IUserController):
 	def __init__(self, user_repository: IUserRepository) -> None:
 		self._user_repository = user_repository
-	
-	def get_users(self) -> Coroutine[Any, Any, List[users]]:
-		return self._user_repository.retrieve_users()
+
+	async def get_users(self) -> Response:
+		promise: list[dict[str, Any]] = []
+
+		try:
+			promise = await self._user_repository.retrieve_users()
+		except Exception as error:
+			print(f"Coudln't JSONIFY users due to an error: {error}")
+
+		return jsonify(promise)
