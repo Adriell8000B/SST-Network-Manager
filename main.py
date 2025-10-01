7365d52e189320998efde211d0f500a18ab7c7a4
Adriell8000B <adrielluiz73@gmail.com>
2025-10-01 10:44:19 -0300

    Replace blueprint by controller

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from flask import Flask
 from asyncio import run
+
+from flask import Flask
 from prisma import Prisma
+
 from controllers.user_controller import UserController
 from database.database import Database
+from repositories.user_repository import UserRepository
 from router.router import Router
-from router.routes import blueprint
 from server.server import Server
 from utils.utils import get_env_string, setup_environment
-from repositories.user_repository import UserRepository
 
 setup_environment()
 
@@ -16,7 +17,7 @@ prisma = Prisma()
 
 user_repository = UserRepository(prisma)
 user_controller = UserController(user_repository)
-router = Router(flask, blueprint)
+router = Router(flask, user_controller)
 database = Database(prisma)
 
 server = Server(
