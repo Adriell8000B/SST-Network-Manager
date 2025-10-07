58e6376596b741ce27db68f83978abda0140abed
Adriell8000B <adrielluiz73@gmail.com>
2025-10-06 23:38:25 -0300

    Finish controller implementation

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -1,5 +1,5 @@
-from typing import Any
-from flask import Response, jsonify
+from typing import Any, Union
+from flask import Response, jsonify, request
 
 from abstracts.abstracts import IUserController, IUserRepository
 
@@ -16,3 +16,14 @@ class UserController(IUserController):
 			print(f"Coudln't JSONIFY users due to an error: {error}")
 
 		return jsonify(promise)
+
+	async def add_user(self) -> Union[None, str]:
+		try:
+			user_name = request.form["username"]
+			user_password = request.form["password"]
+
+			await self._user_repository.create_user(user_name, user_password)
+
+			return None
+		except Exception as error:
+			return error.__class__.__name__
